bc1f7b111ab78edde8a71347a444265749234477
Piotr Banasik <piotr.banasik@gmail.com>
2017-03-09 09:21:34 +0800

    Tweaking excel matcher

--- lib/excel_templating/rspec_excel_matcher.rb
+++ lib/excel_templating/rspec_excel_matcher.rb
@@ -17,8 +17,14 @@ RSpec::Matchers.define :match_excel_content do |expected_excel_path|
   end
 
   failure_message do |excel_path|
-    "expected excel:#{expected_excel_path} to exactly match the content of #{excel_path} but did not.\n" +
-      @excel_matcher.errors.join("\n") + "\n"
+    messages = ["expected excel:#{expected_excel_path} to exactly match the content of #{excel_path} but did not."]
+    @excel_matcher.errors.group_by(&:first).each do |sheet_name, errors|
+      messages << "#{sheet_name}:"
+      errors.each do |_, error|
+        messages << "  #{error}:"
+      end
+    end
+    messages.join("\n")
   end
 
   failure_message_when_negated do |excel_path|
@@ -41,17 +47,17 @@ class RSpec::Matchers::ExcelMatcher
 
   private
 
-  def check(check_result, fail_msg)
+  def check(sheet_name, check_result, fail_msg)
     if check_result
       true
     else
-      errors << fail_msg
+      errors << [sheet_name, fail_msg]
       false
     end
   end
 
   def sheet_count_equal?
-    check(expected_roo.sheets.count == actual_roo.sheets.count, "Number of sheets do not match.")
+    check(:base, expected_roo.sheets.count == actual_roo.sheets.count, "Number of sheets do not match.")
   end
 
   def all_sheets_equal?
@@ -75,21 +81,21 @@ class RSpec::Matchers::ExcelMatcher
         nil
       end
 
-      check(actual, "Sheet names do not match.") if actual != nil
-      check(expected.first_row == actual.first_row, "Number of rows do not match.")
-      check(expected.last_row == actual.last_row, "Number of rows do not match")
-      check(expected.first_column == actual.first_column, "Number of columns do not match")
-      check(expected.last_column == actual.last_column, "Number of columns do not match")
-      check_cells(expected, actual)
+      check(sheet_name, actual, "Sheet names do not match.") if actual != nil
+      # check(sheet_name, expected.first_row == actual.first_row, "Number of rows do not match.")
+      # check(sheet_name, expected.last_row == actual.last_row, "Number of rows do not match")
+      # check(sheet_name, expected.first_column == actual.first_column, "Number of columns do not match")
+      # check(sheet_name, expected.last_column == actual.last_column, "Number of columns do not match")
+      check_cells(sheet_name, expected, actual)
 
       errors.empty?
     end
   end
 
-  def check_cells(sheet1, sheet2)
+  def check_cells(sheet_name, sheet1, sheet2)
     (sheet1.first_row .. sheet1.last_row).each do |row|
       (sheet1.first_column .. sheet1.last_column).each do |col|
-        check(sheet1.cell(row, col) == sheet2.cell(row, col),
+        check(sheet_name, sheet1.cell(row, col) == sheet2.cell(row, col),
               discrepancy_message(col, row, sheet1, sheet2))
       end
     end
